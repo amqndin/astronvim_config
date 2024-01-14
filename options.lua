d458c14e83d2ba4f70baa386b1b7365cf815fae7
amandin <123622161+amawdiin@users.noreply.github.com>
2024-01-15 01:33:45 +0600

    move vim options to options.lua from init.lua

--- options.lua
+++ options.lua
@@ -19,6 +19,9 @@ return {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
   },
+  o = {
+    guifont = "JetBrainsMono Nerd Font:h14"
+  },
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
