1305a26b74a95fe15240778cf9b0a46892c7c8d4
amandin <123622161+amawdiin@users.noreply.github.com>
2024-01-15 13:39:54 +0600

    i dont wanna write a message here.

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,6 +11,7 @@ return {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
